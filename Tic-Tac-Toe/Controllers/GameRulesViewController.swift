d613093a5b5e8bd0933e9e8bbb55de00faaf4afe
Avinash Reddy <beavinash15@gmail.com>
2018-06-19 20:17:02 -0400

    User Interface Setup done for portrait.

---
+++ Tic-Tac-Toe/Controllers/GameRulesViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GameRulesViewController.swift
+//  Tic-Tac-Toe
+//
+//  Created by Avinash Reddy on 6/19/18.
+//  Copyright © 2018 Avinash Reddy. All rights reserved.
+//
+
+import UIKit
+
+class GameRulesViewController: UIViewController {
+    
+    // MARK: - Initial
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Memory managment
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Tic-Tac-Toe/Controllers/GameViewController.swift
+++ Tic-Tac-Toe/Controllers/GameViewController.swift
@@ -9,13 +9,40 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
+    @IBOutlet weak var playerXScoreLabel: UILabel!
+    @IBOutlet weak var playerOScoreLabel: UILabel!
+    @IBOutlet weak var playerTurnLabel: UILabel!
+    
+    // This variable will keep track of player turns in a game
+    var playerTurn: Int = 1
+    
+    
+    // MARK: - Initial
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    
+    // MARK: - Tic Tac Toe custom functions
+    @IBAction func playButtonPressed(_ sender: AnyObject) {
+        if playerTurn == 1 {
+            sender.setImage(UIImage(named: "X"), for: [])
+            playerTurn = 2
+        } else {
+            sender.setImage(UIImage(named: "O"), for: UIControlState.normal)
+            playerTurn = 1
+        }
+    }
+    
+    @IBAction func restartGamePressed(_ sender: Any) {
+        
+    }
+    
+    
+    
+    // MARK: - Memory managment
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
