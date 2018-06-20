d2a1e3749861c91e438815fc15495816d8ed9cc8
Avinash Reddy <beavinash15@gmail.com>
2018-06-20 18:21:33 -0400

    Game logic complete. Now refactoring code should be done.

--- Tic-Tac-Toe/Controllers/GameViewController.swift
+++ Tic-Tac-Toe/Controllers/GameViewController.swift
@@ -14,8 +14,26 @@ class GameViewController: UIViewController {
     @IBOutlet weak var playerOScoreLabel: UILabel!
     @IBOutlet weak var playerTurnLabel: UILabel!
     
-    // This variable will keep track of player turns in a game
+    // This variable will keep track of a player turn in a game
     var playerTurn: Int = 1
+    
+    // This variable shows the winning combinations Tic Tac Toe game can have
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                               [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                               [0, 4, 8], [2, 4, 6]]
+    
+    
+    var playerXScore = 0
+    var playerOScore = 0
+    
+    // Game state
+    var gameStart: Bool = true
+    
+    // Player Turns count
+    var numberOfTurns = 1
+    
+    // Tic Tac Toe board filled
+    var filledCanvas = Array(repeating: 0, count: 9)
     
     
     // MARK: - Initial
@@ -27,17 +45,81 @@ class GameViewController: UIViewController {
     
     // MARK: - Tic Tac Toe custom functions
     @IBAction func playButtonPressed(_ sender: AnyObject) {
-        if playerTurn == 1 {
-            sender.setImage(UIImage(named: "X"), for: [])
-            playerTurn = 2
+        
+        let canvasIndex = sender.tag - 1
+        
+        if filledCanvas[canvasIndex] == 0 && gameStart {
+            
+            filledCanvas[canvasIndex] = playerTurn
+            
+            if playerTurn == 1 {
+                sender.setImage(UIImage(named: "X"), for: [])
+            } else {
+                sender.setImage(UIImage(named: "O"), for: [])
+            }
+            
+            playerTurn *= -1
+            numberOfTurns += 1
+            playerTurnLabel.text = "Player \((playerTurn + 3) % 3 )'s turn"
+            
+            
+            let row = canvasIndex / 3
+            let column = canvasIndex % 3
+            
+            var sum1 = 0, sum2 = 0
+            for i in 0...2 {
+                sum1 += filledCanvas[i * 3 + column]
+                sum2 += filledCanvas[row * 3 + i]
+            }
+            
+            if abs(sum1) == 3 || abs(sum2) == 3 {
+                showResult()
+            }
+            
+            if (row + column) % 2 == 0 {
+                sum1 = filledCanvas[0] + filledCanvas[4] + filledCanvas[8]
+                sum2 = filledCanvas[2] + filledCanvas[4] + filledCanvas[6]
+                
+                if abs(sum1) == 3 || abs(sum2) == 3 {
+                    showResult()
+                }
+            }
+            
+            if (numberOfTurns > 9 && gameStart) {
+                let alert = UIAlertController(title: "Winner", message: "It's a drawn game", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+        }
+    }
+    
+    func showResult() {
+        gameStart = false
+        
+        if playerTurn == -1  {
+            let alert = UIAlertController(title: "Winner", message: "Player 1 Won", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         } else {
-            sender.setImage(UIImage(named: "O"), for: UIControlState.normal)
-            playerTurn = 1
+            let alert = UIAlertController(title: "Winner", message: "Player 2 Won", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
     }
     
     @IBAction func restartGamePressed(_ sender: Any) {
+        playerTurn = 1
+        playerTurnLabel.text = "Player \(playerTurn)'s turn"
+        gameStart = true
+        filledCanvas = Array(repeating: 0, count: 9)
+        numberOfTurns = 1
         
+        for index in 1...9 {
+            if let button = view.viewWithTag(index) as? UIButton {
+                button.setImage(nil, for: UIControlState.normal)
+            }
+        }
     }
     
     
